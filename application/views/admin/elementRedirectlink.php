ee6fffe9a589e7aa7d0bd9b8687ad0ff3ea71977
Hoàng Hà Duy <duyhh@irender.vn>
2020-03-31 02:52:25 +0700

    update

--- application/views/admin/elementRedirectlink.php
+++ application/views/admin/elementRedirectlink.php
@@ -18,28 +18,49 @@
 				<div class="form-group">
 					<label for="redirectLink">Liên kết <?php echo $moduleName; ?> </label>
 					<div class="input-group">
-						<div class="input-group-addon"><i class="fa fa-link"></i> Nhập liên kết</div>
-						<input class="form-control" id="redirectLink" placeholder="https://link-to-file/download"/>
+						<div class="input-group-addon"><i class="fa fa-link"></i> Nhập liên kết&nbsp;&nbsp;</div>
+						<input class="form-control" id="redirectLink" placeholder="https://link-to-file/download"
+							style="font-size: 18px;"/>
+						<div class="input-group-btn">
+							<button type="button" class="btn btn-lg btn-primary" id="browseFileInput"
+								style="padding: 1rem 3rem !important;">
+								<i class="fa fa-link"></i> Browse
+							</button>
+						</div>
 					</div>	
 				</div>	 
-				<h3><strong>Hoặc tải lên file</strong></h3>
-				<div class="form-group">
-					<label for="redirectLink">Tải file lên cho liên kết <?php echo $moduleName; ?> </label>
-					<div class="box">
-						<input type="file" name="userfile" id="file-1" class="inputfile inputfile-1" data-multiple-caption="{count} files selected" multiple />
-						<label for="file-1"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="17" viewBox="0 0 20 17"><path d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"/></svg> <span>Choose a file&hellip;</span></label>
-					</div>
-				</div>	 
-				<hr/>
-				<div class="form-group">
-					<button type="submit" class="btn btn-lg btn-success"><i class="fa fa-floppy-o"></i> Save</button>
-				</div>  
 			</div>
 		</form>
     </div>
 </div>
 
+<script>
+	function selectFileWithCKFinder( elementId ) {
+		CKFinder.popup( {
+			chooseFiles: true,
+			width: 800,
+			height: 600,
+			onInit: function( finder ) {
+				finder.on( 'files:choose', function( evt ) {
+					var file = evt.data.files.first();
+					var output = document.getElementById( elementId );
+					output.value = file.getUrl();
+				} );
 
+				finder.on( 'file:choose:resizedImage', function( evt ) {
+					var output = document.getElementById( elementId );
+					output.value = evt.data.resizedUrl;
+				} );
+			}
+		} );
+	}
+	$(function(){
+
+		$('#browseFileInput').click(() =>{
+			selectFileWithCKFinder( 'redirectLink' );
+		});
+	});
+</script>
 <link rel="stylesheet" type="text/css" href="<?=base_url()?>/css/normalize.css" />
 <style lang="css">
     
